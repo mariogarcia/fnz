273c00580940036612e329eed1459b404302be59
Mario Garcia <mario.ggar@gmail.com>
2014-04-28 21:01:57 +0200

    Added Where clause with @CompileStatic

---
+++ src/main/groovy/fnz/control/Where.groovy
@@ -0,0 +1,83 @@
+package fnz.control
+
+import static org.codehaus.groovy.runtime.DefaultGroovyMethods.find
+import static org.codehaus.groovy.runtime.DefaultGroovyMethods.collect
+
+import org.codehaus.groovy.runtime.ComposedClosure
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class Where {
+
+    private static class Evaluation {
+        Closure condition
+        Closure execution
+    }
+
+    private List<Evaluation> conditions = []
+    private Map<?, ?> parameters = [:]
+
+    private Closure<Closure<Evaluation>>  applyDelegateToCondition = { final parameters ->
+        return { final Evaluation evaluation ->
+            evaluation.condition.delegate = parameters
+            evaluation
+        }
+    }
+
+    private Closure<Closure<Evaluation>>  applyDelegateToExecution = { final parameters ->
+       return { final Evaluation evaluation ->
+            evaluation.execution.delegate = parameters
+            evaluation
+       }
+    }
+
+    private Closure<Boolean>  firstTrue = { Evaluation evaluation ->
+        evaluation.condition.call()
+    }
+
+    def when(Closure cl) {
+        def createCondition = { Closure closure -> new Evaluation(condition: closure)}
+        conditions << createCondition(cl)
+        return this
+    }
+
+    def rightShift(Closure value) {
+        then(value)
+    }
+
+    def then(Closure cl) {
+        conditions.last().execution = cl
+        return this
+    }
+
+    def otherwise(Closure cl) {
+        conditions << new Evaluation(condition: {true}, execution: cl)
+    }
+
+    def where (Closure whereClause) {
+        this.with(whereClause)
+        evaluate()
+    }
+
+    def propertyMissing(String name, value) {
+        parameters[name] = value
+    }
+
+    def evaluate() {
+        Closure<Evaluation> byApplyingParameters = composedClosure.call(parameters)
+        Closure execution = find(collect(conditions, byApplyingParameters), firstTrue).execution
+
+        return execution.call()
+    }
+
+    private ComposedClosure<Closure<Evaluation>> getComposedClosure() {
+       return new ComposedClosure(applyDelegateToCondition, applyDelegateToExecution)
+    }
+
+    public static Object check(Map values, Closure<Evaluation> evaluation) {
+        def where = new Where()
+        where.parameters = values
+        where.with(evaluation)
+    }
+
+}
---
+++ src/test/groovy/fnz/control/WhereSpecification.groovy
@@ -0,0 +1,25 @@
+package fnz.control
+
+import spock.lang.Specification
+
+class WhereSpecificatione extends Specification {
+
+    void 'Build a where expression'() {
+        given: 'An expression'
+            def values = [weight:60]
+            def result = Where.check(values) {
+                when { weight <= underweight } then { "You're underweight" }
+                when { weight <= normal }      then { "You're normal" }
+                when { weight <= fat }         then { "You're fat" }
+                otherwise { "You have a strange composition" }
+                where {
+                    underweight = 50
+                    normal      = 70
+                    fat         = 90
+                }
+            }
+        expect:
+            result == "You're underweight"
+    }
+
+}
