bb74368464a15568a88530ea037a29e4ffd1f7c2
Mario Garcia <mario.ggar@gmail.com>
2014-05-05 19:06:26 +0200

    Adding comments to Let

--- src/main/groovy/fnz/control/Let.groovy
+++ src/main/groovy/fnz/control/Let.groovy
@@ -2,6 +2,12 @@ package fnz.control
 
 import fnz.base.Option
 
+/**
+ * This type of expression is inspired in Clojure's let macro
+ *
+ * @author @marioggar
+ * @since 0.1
+ */
 class Let<T> {
 
     static <T> Option<T> let(Map initValues, Closure<T> execution) {
