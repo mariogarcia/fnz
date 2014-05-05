f8751480966c29e7fa12cf6787581d253f6cf984
Mario Garcia <mario.ggar@gmail.com>
2014-05-06 00:56:58 +0200

    Let works with @CompileStatic

--- src/main/groovy/fnz/control/Let.groovy
+++ src/main/groovy/fnz/control/Let.groovy
@@ -1,8 +1,10 @@
 package fnz.control
 
+import static org.codehaus.groovy.runtime.DefaultGroovyMethods.with
 import static org.codehaus.groovy.runtime.DefaultGroovyMethods.inject
 
 import fnz.base.Option
+import groovy.transform.CompileStatic
 
 /**
  * This type of expression is inspired in Clojure's let macro
@@ -10,25 +12,21 @@ import fnz.base.Option
  * @author @marioggar
  * @since 0.1
  */
+@CompileStatic
 class Let<T> {
 
-    static <T> Option<T> let(Map initValues, Closure<T> execution) {
+    static final Closure<Map<?,?>> aggregator = { Map acc, Map.Entry entry ->
+        acc.get(
+            entry.key,
+            entry.value instanceof Closure ?
+                with(acc, (Closure) entry.value) :
+                entry.value
+        )
+        return acc
+    }
 
-        Closure<Map<?,?>> aggregator = { Map acc, Map.Entry entry ->
-            acc.get(
-                entry.key,
-                entry.value instanceof Closure ?
-                    acc.with(entry.value) :
-                    entry.value
-            )
-            return acc
-        }
-
-        Map<?,?> evaluatedInitValues =
-            inject(initValues, [:], aggregator)
-
-
-        return Option.of(evaluatedInitValues.with(execution))
+    static <T> Option<T> let(final Map<?,?> initValues, final Closure<T> execution) {
+        return Option.of(with(inject(initValues, [:], aggregator), execution))
     }
 
 }
