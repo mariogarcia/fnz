375439bd8a8a732d30e36ef931af531e98c98a05
Mario Garcia <mario.ggar@gmail.com>
2014-05-29 15:22:03 +0200

    Adding more cases to where specification

--- src/test/groovy/fnz/control/WhereSpecification.groovy
+++ src/test/groovy/fnz/control/WhereSpecification.groovy
@@ -10,11 +10,11 @@ class WhereSpecificatione extends Specification {
 
     @Unroll
     void 'Build a where expression'() {
-        given: 'An expression'
+        when: 'An expression'
             // tag::simpleWhere[]
             Option<String> result = check(weight: value) {
                 when { weight <= underweight } then { "You're underweight" }
-                when { weight <= normal }      then { "You're normal" }
+                when { weight <= normal }      then { throw new Exception("You're normal") }
                 when { weight <= fat }         then { "You're fat" }
                 otherwise { "You have a strange composition" }
                 where {
@@ -24,14 +24,33 @@ class WhereSpecificatione extends Specification {
                 }
             }
             // end::simpleWhere[]
-        expect: "The underweight output"
+        then: 'Not exception has been thrown'
+            notThrown(Exception)
+        and: "The underweight output"
             result.isPresent() == true
             result.get() == expected
         where:
             value | expected
-            45    | "You're underweight"
-            70    | "You're normal"
-            85    | "You're fat"
+            50    | "You're underweight"
+            90    | "You're fat"
+    }
+
+    @Unroll
+    void 'Throwing an exception'() {
+        when: 'An expression'
+            Option<String> result = check(weight: 70) {
+                when { weight <= underweight } then { "You're underweight" }
+                when { weight <= normal }      then { throw new Exception("You're normal") }
+                when { weight <= fat }         then { "You're fat" }
+                otherwise { "You have a strange composition" }
+                where {
+                    underweight = 50
+                    normal      = 70
+                    fat         = 90
+                }
+            }
+        then: 'An exception has been thrown'
+            thrown(Exception)
     }
 
     @Unroll
