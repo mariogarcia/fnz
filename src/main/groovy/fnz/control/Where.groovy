169896e02fc3e29deeff03dd30d1f738184a5037
Mario Garcia <mario.ggar@gmail.com>
2014-06-04 22:39:34 +0200

    Getting rid of find and collect static method references from DefaultGroovyMethods

--- src/main/groovy/fnz/control/Where.groovy
+++ src/main/groovy/fnz/control/Where.groovy
@@ -1,8 +1,6 @@
 package fnz.control
 
-import static org.codehaus.groovy.runtime.DefaultGroovyMethods.find
 import static org.codehaus.groovy.runtime.DefaultGroovyMethods.isCase
-import static org.codehaus.groovy.runtime.DefaultGroovyMethods.collect
 
 import org.codehaus.groovy.runtime.ComposedClosure
 import groovy.transform.CompileStatic
@@ -26,8 +24,62 @@ class Where {
     private List<Evaluation> conditions = []
     private Map<?, ?> parameters = [:]
 
-    private Where(Map<?,?> parameters) {
+    private Where(final Map<?,?> parameters) {
         this.parameters = parameters
+    }
+
+    Where when(final Closure cl) {
+        conditions << new Evaluation(condition: cl)
+        return this
+    }
+
+    Where when(final Class clazz) {
+        conditions << new Evaluation(condition: { isCase(clazz, parameters.val) })
+        return this
+    }
+
+    Where when(final Collection collection) {
+        conditions << new Evaluation(condition: { isCase(collection, parameters.val) })
+        return this
+    }
+
+    Where when(final Number number) {
+        conditions << new Evaluation(condition: { isCase(number, parameters.val) })
+        return this
+    }
+
+    Where when(final Map map) {
+        conditions << new Evaluation(condition: { isCase(map, parameters.val) })
+        return this
+    }
+
+    Where then(final Closure cl) {
+        conditions.last().execution = cl
+        return this
+    }
+
+    Where otherwise(final Closure cl) {
+        conditions << new Evaluation(condition: {true}, execution: cl)
+        return this
+    }
+
+    Where where (final Closure whereClause) {
+        this.with(whereClause)
+        return this
+    }
+
+    def propertyMissing(String name, value) {
+        parameters.get(name,value)
+    }
+
+    def evaluate() {
+        Closure<Evaluation> byApplyingParameters = composedClosure.call(parameters)
+        Closure execution =
+            conditions.
+                collect(byApplyingParameters).
+                find(firstTrue).execution
+
+        return execution.call()
     }
 
     private Closure<Closure<Evaluation>>  applyDelegateToCondition = { final parameters ->
@@ -48,71 +100,19 @@ class Where {
         evaluation.condition.call()
     }
 
-    def when(final Closure cl) {
-        conditions << new Evaluation(condition: cl)
-        return this
-    }
-
-    def when(final Class clazz) {
-        conditions << new Evaluation(condition: { isCase(clazz, parameters.val) })
-        return this
-    }
-
-    def when(final Collection collection) {
-        conditions << new Evaluation(condition: { isCase(collection, parameters.val) })
-        return this
-    }
-
-    def when(final Number number) {
-        conditions << new Evaluation(condition: { isCase(number, parameters.val) })
-        return this
-    }
-
-    def when(final Map map) {
-        conditions << new Evaluation(condition: { isCase(map, parameters.val) })
-        return this
-    }
-
-    def then(final Closure cl) {
-        conditions.last().execution = cl
-        return this
-    }
-
-    def otherwise(final Closure cl) {
-        conditions << new Evaluation(condition: {true}, execution: cl)
-        return this
-    }
-
-    def where (final Closure whereClause) {
-        this.with(whereClause)
-        return this
-    }
-
-    def propertyMissing(String name, value) {
-        parameters.get(name,value)
-    }
-
-    def evaluate() {
-        Closure<Evaluation> byApplyingParameters = composedClosure.call(parameters)
-        List<Evaluation> evaluatedConditions = collect(conditions, byApplyingParameters)
-        Closure execution = find(evaluatedConditions, firstTrue).execution
-
-        return execution.call()
-    }
-
     private ComposedClosure<Closure<Evaluation>> getComposedClosure() {
        return new ComposedClosure(applyDelegateToCondition, applyDelegateToExecution)
     }
 
-    public static <T> Option<T> check(Map values, Closure<Evaluation> evaluation) {
+    public static <T> Option<T> check(final Map values, final Closure<Evaluation> evaluation) {
         return _check(values ?: [:], evaluation)
     }
 
-    public static <T> Option<T> check(Object value, Closure<Evaluation> evaluation) {
+    public static <T> Option<T> check(final Object value, final Closure<Evaluation> evaluation) {
         return _check([val:value], evaluation)
     }
 
-    private static <T> Option<T> _check(Object value, Closure<Evaluation> evaluation) {
+    private static <T> Option<T> _check(final Object value, final Closure<Evaluation> evaluation) {
         def where = new Where((Map) value)
         where.with(evaluation)
         return Option.of(where.evaluate())
