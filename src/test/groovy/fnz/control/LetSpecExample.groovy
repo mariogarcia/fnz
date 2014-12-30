ef6b4677207a8ed95563445664ee856ed8a1918d
Mario Garcia <mario.ggar@gmail.com>
2014-12-30 20:23:42 +0100

    Fixing let documentation

--- src/test/groovy/fnz/control/LetSpecExample.groovy
+++ src/test/groovy/fnz/control/LetSpecExample.groovy
@@ -2,12 +2,23 @@ package fnz.control
 
 class LetSpecExample {
 
+    // tag::simpleLet[]
     Integer simpleLetExample() {
+        let(x: 1, y: 2,  z: 3) {
+            x + y + z
+        }
+    }
+    // end::simpleLet[]
+
+    // tag::computedValues[]
+    Integer computedValues() {
         let(x: 1, y: 2,  z: { x + y }) {
             x + y + z
         }
     }
+    // end:computedValues[]
 
+    // tag::nestedLet[]
     Integer nestedLetExample() {
         let(a: 5, b: 15) {
             let(c: 15, d: { a + 25 }) {
@@ -15,5 +26,6 @@ class LetSpecExample {
             }
         }
     }
+    // end::nestedLet[]
 
 }
