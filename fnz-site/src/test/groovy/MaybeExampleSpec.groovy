dec884625a78891c41ab09976f0e526958cbe55b
Mario Garcia <mario.ggar@gmail.com>
2015-06-30 00:04:12 +0200

    Add more examples and prepare external links

--- fnz-site/src/test/groovy/MaybeExampleSpec.groovy
+++ fnz-site/src/test/groovy/MaybeExampleSpec.groovy
@@ -5,24 +5,58 @@ import spock.lang.Specification
 
 class MaybeExampleSpec extends Specification {
 
-    // tag::maybeBasic1[]
     void 'handling NPE with Maybe'() {
-        when: 'having a null reference'
-            Integer value = Fnz.Maybe(null).or(Fnz.Just(0)).get() + 1
-
-        then: 'we dont have to worry about NPE'
-            value == 1
+        expect: 'having a null reference'
+        // tag::maybeBasic1[]
+        assert 1 == Fnz.Maybe(null).or(Fnz.Just(0)).get() + 1
+        // end::maybeBasic1[]
     }
-    // end::maybeBasic1[]
 
-    // tag::maybeBasic2[]
     void 'handling NPE with Maybe without import'() {
-        when: 'having a null reference'
-            Integer value = Maybe(null).or(Just(0)).get() + 1
-
-        then: 'we dont have to worry about NPE'
-            value == 1
+        expect: 'having a null reference'
+        // tag::maybeBasic2[]
+        assert 1 == Maybe(null).or(Just(0)).get() + 1
+        // end::maybeBasic2[]
     }
-    // end::maybeBasic2[]
+
+    void 'handling NPE with actions'() {
+        expect: 'having a null reference'
+        // tag::maybeBasic3[]
+        assert Maybe(null)
+               .or(Just(0))
+               .fmap { x -> x + 1 }
+               .fmap { y -> y + 1 }
+               .get() == 2
+        // end::maybeBasic3[]
+    }
+
+    void 'handling NPE with actions and Elvis'() {
+        expect: 'having a null reference'
+        // tag::maybeBasic4[]
+        assert (null ?: 0)
+            .collect { x -> x + 1 }
+            .collect { y -> y + 1 }
+            .find() == 2
+        // end::maybeBasic4[]
+    }
+
+    // tag::maybeBasic5[]
+    void 'invoking a method depending on Maybe'() {
+        when: 'having a null reference'
+        def number = Maybe(null) // == Nothing()
+        def result = execute(number)
+
+        then: 'we should get a bad result'
+        result == 'bad'
+    }
+
+    String execute(fnz.data.Maybe.Nothing nothing) {
+        return 'bad'
+    }
+
+    String execute(fnz.data.Maybe.Just value) {
+        return 'good'
+    }
+    // end::maybeBasic5[]
 
 }
